321969ad7d474ead00eabf768553f5e2dc903d0b
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-13 23:55:10 +0530

    feat: add server action response type

---
+++ lib/types/server-action.ts
@@ -0,0 +1,9 @@
+type SuccessResponse<T> = {
+  success: true;
+  data: T;
+  
+}
+
+type ErrorResponse = {success: false, error: string};
+
+export type ServerActionResponse<T> = Promise<SuccessResponse<T> | ErrorResponse>
