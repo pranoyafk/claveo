34c3f23d6def0dac40ee7c165adbe326b66299af
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-13 12:06:24 +0530

    fix(auth): add auth schema into drizzle client

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,4 +1,10 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { env } from '../env';
+import {account, session, user, verification} from './schema/auth'
 
-export const db = drizzle(env.DATABASE_URL);
+export const db = drizzle(env.DATABASE_URL, {schema: {
+    user,
+    account, 
+    verification,
+    session
+}});
