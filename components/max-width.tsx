0745f6e8d7c45848c521f2ab6586d99d0e938d37
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-13 23:58:07 +0530

    feat: max width wrapper as a reusable component

---
+++ components/max-width.tsx
@@ -0,0 +1,22 @@
+// components/MaxWidthWrapper.tsx
+
+import clsx from "clsx";
+import type { ReactNode } from "react";
+
+interface MaxWidthWrapperProps {
+  className?: string;
+  children: ReactNode;
+}
+
+export function MaxWidthWrapper({ className, children }: MaxWidthWrapperProps) {
+  return (
+    <div
+      className={clsx(
+        "mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
