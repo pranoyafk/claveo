02cf2a6dc6a524d79b7240951e2274337e907481
Pranoy Majumdar <works.pranoy@gmail.com>
2025-12-30 22:44:27 +0530

    chore: added ignores

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,17 @@
-// @ts-check
 import { tanstackConfig } from "@tanstack/eslint-config";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-export default [...tanstackConfig, {}, eslintConfigPrettier];
+export default [
+  ...tanstackConfig,
+  {
+    ignores: [
+      "dist",
+      "node_modules",
+      "routeTree.gen.ts",
+      "src/components/ui",
+      "eslint.config.js",
+      "prettier.config.js",
+    ],
+  },
+  eslintConfigPrettier,
+];
