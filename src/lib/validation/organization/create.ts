fc6a1f5556dfed2054676b64c38596b8b1ea562c
Pranoy Majumdar <works.pranoy@gmail.com>
2025-12-30 22:45:35 +0530

    chore: add proper validation using regex

--- src/lib/validation/organization/create.ts
+++ src/lib/validation/organization/create.ts
@@ -1,6 +1,11 @@
 import z from "zod";
 
 export const createOrganizationSchema = z.object({
-  organizationName: z.string().min(5),
-  organizationSlug: z.string(),
+  organizationName: z
+    .string()
+    .min(5)
+    .regex(/^[a-zA-Z0-9 ]+$/, { message: "No special characters allowed" }),
+  organizationSlug: z.string().regex(/^[a-zA-Z0-9]+$/, {
+    message: "Only alphanumeric characters allowed",
+  }),
 });
