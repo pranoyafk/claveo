f8c5a02fe062ec8e521a820b52baf0701e2fb08a
Pranoy Majumdar <works.pranoy@gmail.com>
2026-01-16 00:15:59 +0530

    feat: clickable project card

--- src/features/projects/components/project-card.tsx
+++ src/features/projects/components/project-card.tsx
@@ -1,3 +1,7 @@
+import { IconCalendarEvent, IconFlag3, IconListCheck } from "@tabler/icons-react";
+import { Link } from "@tanstack/react-router";
+import { ProjectCardAction } from "./project-card-action";
+import type { Project } from "@/lib/db/schema";
 import { Badge } from "@/components/ui/badge";
 import {
   Card,
@@ -9,15 +13,22 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
-import type { Project } from "@/lib/db/schema";
-import { IconCalendarEvent, IconFlag3, IconListCheck } from "@tabler/icons-react";
-import { ProjectCardAction } from "./project-card-action";
+import { useActiveOrganization } from "@/features/organizations/hooks/use-active-organization";
 
 type ProjectCardProps = { project: Project };
 
 export function ProjectCard(props: ProjectCardProps) {
+  const activeOrg = useActiveOrganization();
   return (
-    <Card>
+    <Card className="relative">
+      <Link
+        to="/app/$organizationSlug/$projectSlug"
+        params={{
+          organizationSlug: activeOrg.slug,
+          projectSlug: "hehe",
+        }}
+        className="absolute z-10 inset-0"
+      />
       <CardHeader>
         <CardTitle className="line-clamp-1" title={props.project.name}>
           {props.project.name}
@@ -25,7 +36,7 @@ export function ProjectCard(props: ProjectCardProps) {
         <CardDescription className="line-clamp-2 h-10 mt-1.5">
           {props.project.description || "Add a short project description"}
         </CardDescription>
-        <CardAction>
+        <CardAction className="relative z-20">
           <ProjectCardAction projectSlug={props.project.slug} />
         </CardAction>
       </CardHeader>
