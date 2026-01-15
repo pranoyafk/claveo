e3e68d7ca6ba8169190e27891ce0638ac88d90f9
Pranoy Majumdar <works.pranoy@gmail.com>
2026-01-16 00:22:51 +0530

    chore: organize imports

--- src/routes/app/$organizationSlug/index.tsx
+++ src/routes/app/$organizationSlug/index.tsx
@@ -1,6 +1,3 @@
-import { IconFolderPlus } from "@tabler/icons-react";
-import { useSuspenseQuery } from "@tanstack/react-query";
-import { createFileRoute } from "@tanstack/react-router";
 import { Page, PageAction, PageContent, PageDescription, PageHeader, PageTitle } from "@/components/page";
 import { Button } from "@/components/ui/button";
 import { CreateProjectDialog } from "@/features/projects/components/create-project-dialog";
@@ -8,6 +5,9 @@ import { ProjectsEmptyState } from "@/features/projects/components/empty-state";
 import { ProjectCard } from "@/features/projects/components/project-card";
 import { projectsQueries } from "@/features/projects/queries";
 import { useIsMobile } from "@/hooks/use-mobile";
+import { IconFolderPlus } from "@tabler/icons-react";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/app/$organizationSlug/")({
   component: RouteComponent,
@@ -17,6 +17,8 @@ function RouteComponent() {
   const { activeOrg } = Route.useRouteContext();
   const { data: projects } = useSuspenseQuery(projectsQueries.byOrganization(activeOrg.slug));
   const isMobile = useIsMobile();
+
+  
   return (
     <Page>
       <PageHeader>
